b70e0680cb1b76b41ab847f4c78fa58e41d70acc
Janis Salins <janis.salins@gmail.com>
2023-01-26 19:14:58 +0200

    added astro compress and team animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://j-salins.github.io',
-  base: '/mccann',
+  integrations: [tailwind(), compress()]
+  // site: 'https://j-salins.github.io',
+  // base: '/mccann',
 });
